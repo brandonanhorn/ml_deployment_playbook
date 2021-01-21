c0c886842a6ef20a97167814e865636f4feeb932
brandonanhorn <38738274+brandonanhorn@users.noreply.github.com>
2021-01-21 15:53:21 -0700

    updated to use a pipe

--- data_and_model/data.py
+++ data_and_model/data.py
@@ -5,16 +5,19 @@ import random
 ## creating a df with random numbers
 anything = ["blue", "car", "house", "sport"]
 
-df = pd.DataFrame(np.random.randint(0,51,size=(100, 4)), columns=list('ABCD'))
+df = pd.DataFrame(np.random.randint(0,51,size=(1000, 4)), columns=list('ABCD'))
 
 randomize = []
-for i in range(100):
+for i in range(1000):
     random_choice = random.choice(anything)
     randomize.append(random_choice)
 
 df["E"] = randomize
 
 df.head(3)
+
+#saving the created data to a csv
+df.to_csv("data/the_data.csv")
 
 ## try to predict column d
 target = "D"
@@ -61,3 +64,19 @@ history = model.fit(Z_train, y_train,
                     validation_data=(Z_test, y_test),
                     epochs=5, batch_size=1,
                     verbose=2)
+
+model.save("model.h5")
+
+loaded_model = load_model("model.h5")
+from sklearn.pipeline import make_pipeline
+
+pipe = make_pipeline(mapper, loaded_model)
+pipe.fit(X_train, y_train)
+
+new = pd.DataFrame({
+    'A': [11],
+    'B': [15],
+    'C': [27],
+    'E': "house"})
+
+pipe.predict(new)
